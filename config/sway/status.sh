ba96a3e98972d1c0735e609e520c1e4c87eb6b40
gentootoaster <lobachev.arseniy@gmail.com>
2025-04-29 17:04:50 +0500

    Update status.sh

--- config/sway/status.sh
+++ config/sway/status.sh
@@ -12,25 +12,25 @@ keyboard=$(layout=$(swaymsg -t get_inputs | grep "xkb_active_layout_name"); if e
 # --- Volume ---
 volume_level=$(pamixer --get-volume)
 is_muted=$(pamixer --get-mute)
-volume=$( [ "$is_muted" = "true" ] && echo "muted" || echo "vol $volume_level%")
+volume=$( [ "$is_muted" = "true" ] && echo "<span color='#ff0000'>muted</span>" || echo "vol $volume_level%")
 
 # --- Bluetooth ---
 bluetooth_powered=$(bluetoothctl show | grep -q "Powered: yes" && echo "yes" || echo "no")
 if [ "$bluetooth_powered" = "yes" ]; then
     connected_devices=$(bluetoothctl devices Connected | wc -l)
     if [ "$connected_devices" -gt 0 ]; then
-        bluetooth="<span color='#0096FF'>bluetooth: ON ($connected_devices)</span>"
+        bluetooth="<span color='#0096FF'>bt ON ($connected_devices)</span>"
     else
-        bluetooth="bluetooth ON"
+        bluetooth="bt ON"
     fi
 else
-    bluetooth="bluetooth OFF"
+    bluetooth="bt OFF"
 fi
 
 # --- RAM Usage ---
 ram_total=$(free -m | awk '/Mem:/ {print $2}')
+ram_used_h=$(free -h | awk '/Mem:/ {print $3}')
 ram_used=$(free -m | awk '/Mem:/ {print $3}')
-ram_free=$(free -m | awk '/Mem:/ {print $7}')
 ram_percent=$(( ram_used * 100 / ram_total ))
 
 if [ $ram_percent -lt 50 ]; then
@@ -41,7 +41,7 @@ else
     ram_color="#ff0000"  
 fi
 
-ram="<span color='$ram_color'>${ram_used}MiB/${ram_free}MiB(${ram_percent}%)</span>"
+ram="<span color='$ram_color'>${ram_used_h}B(${ram_percent}%)</span>"
 
 # --- Network Speed ---
 get_network_speed() {
@@ -67,19 +67,51 @@ active_connection=$(nmcli -t -f NAME,DEVICE,TYPE con show --active | grep -v "lo
 if [[ -n "$active_connection" ]]; then
     network_speed=$(get_network_speed)
     if echo "$active_connection" | grep -q "ethernet"; then
-        net="ETH $network_speed"
+        net="eth $network_speed"
     elif echo "$active_connection" | grep -q "wireless"; then
         SSID=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d':' -f2)
-        net="WiFi $SSID $network_speed"
+        net="wlan $SSID $network_speed"
     fi
 else
-    net="<span color='#ff0000'>No connection</span>"
+    net="<span color='#ff0000'>! no connection</span>"
 fi
 
+# --- Battery ---
+battery=""
+battery_info=$(upower -i $(upower -e | grep BAT) | grep -E 'percentage|state')
+
+if [[ -n "$battery_info" ]]; then
+    battery_percentage=$(echo "$battery_info" | grep 'percentage' | awk '{print $2}' | tr -d '%')
+    battery_state=$(echo "$battery_info" | grep 'state' | awk '{print $2}')
+
+    if (( battery_percentage >= 60 )); then
+        color="#25ad76"
+    elif (( battery_percentage >= 20 )); then
+        color="#ffff00"
+    else
+        color="#ff0000" 
+    fi
+
+    if [[ "$battery_state" == "charging" ]]; then
+        battery="<span color='$color'>chrg $battery_percentage%</span>"
+    elif [[ "$battery_state" == "discharging" ]]; then
+        battery="<span color='$color'>bat $battery_percentage%</span>"
+    else
+        battery="<span color='$color'>? $battery_state</span>"
+    fi
+else
+    battery="<span color='#ff0000'>! No bat info</span>"
+fi
+
+# --- Backlight ---
+backlight=$(brightnessctl | grep 'brightness' | awk '{print $4}' | tr -d '()')
+
 # --- Modules ---
-echo "<span color='#25ad76'>$net</span> | \
-$bluetooth | \
+echo "<span color='#00ff00'>$net</span> | \
 $ram | \
+bri $backlight | \
+$battery | \
+$bluetooth | \
 $volume | \
 $keyboard | \
 $date"
